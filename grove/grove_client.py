cd82ee3a6f39e4092e94cde7e70efc6812905dc7
Yannick Rüfenacht <yannickmawuena.ruefenacht@students.bfh.ch>
2020-12-12 21:14:33 +0100

    Added time limit and exception handling to client

--- grove/grove_client.py
+++ grove/grove_client.py
@@ -8,11 +8,14 @@ import struct
 
 HOST = '192.168.3.10'
 PORT = 65432
+TIMEOUT = 5
 
 class GroveClient:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.settimeout(TIMEOUT)
         self.s.connect((HOST, PORT))
+
     def get_distance(self):
         return self.s.recv(1024).decode()
 
--- grove/space_slide_invaders.py
+++ grove/space_slide_invaders.py
@@ -15,13 +15,19 @@ class App(Frame):
 
     def update_distance(self):
         distance = self.client.get_distance()
+        print(distance)
         self.distance_label.configure(text=str(distance))
         self.after(100, self.update_distance)
 
 root = Tk()
 app = App(root)
-app.set_client(grove_client.GroveClient())
-app.update_distance()
+
+try:
+    client = grove_client.GroveClient()
+    app.set_client(client)
+    app.update_distance()
+except:
+    print("Client connection could not be established")
 
 root.wm_title("Space Slide Invaders")
 root.geometry("500x200")
