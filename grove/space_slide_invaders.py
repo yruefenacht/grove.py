1627c4295d3d82c72e6079de370392812f6e13c0
Yannick Rüfenacht <yannickmawuena.ruefenacht@students.bfh.ch>
2020-12-13 16:41:19 +0100

    Completed space invaders pygame

--- grove/space_slide_invaders.py
+++ grove/space_slide_invaders.py
@@ -13,6 +13,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # Load assets
 PLAYER_SHIP = pygame.image.load(os.path.join("grove", "assets", "space_player_ship.png"))
+ENEMY_SHIP = pygame.image.load(os.path.join("grove", "assets", "space_enemy_ship.png"))
 BG = pygame.transform.scale(pygame.image.load(os.path.join("grove", "assets", "space_bg.png")), (WIDTH,HEIGHT))
 
 class Entity:
@@ -31,22 +32,39 @@ class Entity:
 	def get_height(self):
 		return self.img.get_height()
 		
-
+	def collision(self, other_entity):
+		return collide(self, other_entity)
+		
+def collide(entity1, entity2):
+	offset_x = entity2.x - entity1.x
+	offset_y = entity2.y - entity1.y
+	return entity1.mask.overlap(entity2.mask, (offset_x, offset_y)) != None
+	
 def main():
 	run = True
 	FPS = 60
 	score = 0
 	main_font = pygame.font.SysFont("comicsans", 50)
+	lost_font = pygame.font.SysFont("comicsans", 60)
 	
-	player_vel = 5
+	enemies = []
+	enemies_vel = 3
+	
+	player_vel = 10
 	
 	player = Entity(350, 600, PLAYER_SHIP)
 	
 	clock = pygame.time.Clock()
 	
+	lost = False
+	lost_count = 3
+	
 	client = None
 	
 	try:
+		client_label = main_font.render("Connecting to client...", 1, (255, 255, 255))
+		WIN.blit(client_label, (WIDTH/2 - client_label.get_width()/2, 425))
+		pygame.display.update()
 		client = grove_client.GroveClient()
 	except:
 		print("Client connection could not be established")
@@ -57,18 +75,45 @@ def main():
 		score_label = main_font.render(f"Score: {score}", 1, (255, 255, 255))
 		WIN.blit(score_label, (10, 10))
 		
+		for enemy in enemies:
+			enemy.draw(WIN)
+			
 		player.draw(WIN)
 		
+		if lost:
+			lost_label = lost_font.render("Game Over", 1, (255, 255, 255))
+			WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
+		
 		pygame.display.update()
+	
+	counter = 0
+	enemy_spawn_frequency = 100
 	
 	while run:
 		clock.tick(FPS)
 		redraw_window()
 		
+		if lost:
+			lost_count += 1
+			if lost_count > FPS * 3:
+				run = False
+			else:
+				continue
+		
+		# Spawn Enemies
+		if counter % enemy_spawn_frequency == 0:
+			counter = 0
+			if enemy_spawn_frequency > 1:
+				enemy_spawn_frequency -= 1
+			enemy = Entity(random.randrange(0, WIDTH-50), random.randrange(-1500, -100), ENEMY_SHIP)
+			enemies.append(enemy)
+			
+		counter += 1
+		
 		# Events
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				run = False
+				quit()
 		
 		# Movement keyboard
 		keys = pygame.key.get_pressed()
@@ -81,6 +126,31 @@ def main():
 		if client is not None:
 			distance = self.client.get_distance()
 			player.x = distance
+			
+		# Movement enemies
+		for enemy in enemies[:]:
+			enemy.y += enemies_vel
+			if player.collision(enemy):
+				lost = True
+			if enemy.y + enemy.get_height() > HEIGHT:
+				score += 1
+				enemies.remove(enemy)
+			
+def main_menu():
+	title_font = pygame.font.SysFont("comicsans", 70)
+	run = True
+	while run:
+		WIN.blit(BG, (0, 0))
+		title_label = title_font.render("Press any key to begin...", 1, (255, 255, 255))
+		WIN.blit(title_label, (WIDTH/2 - title_label.get_width()/2, 350))
+		pygame.display.update()
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+			if event.type == pygame.KEYDOWN:
+				main()	
+	pygame.quit()
 				
-main()
+main_menu()
 
